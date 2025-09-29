c6e7767cb0a08d80a4a03d9bf8ca429e887537c0
eriinys <eriinys@duck.com>
2025-09-29 13:26:49 -0500

    .

--- src/main/java/FullNameApplication.java
+++ src/main/java/FullNameApplication.java
@@ -1,4 +1,21 @@
-package PACKAGE_NAME;
+import java.util.*;
 
 public class FullNameApplication {
+    public static void main(String[] args) {
+        Scanner myScanner = new Scanner (System.in);
+
+        String firstName = readString(myScanner, "Enter your first name:");
+        String middleName = readString(myScanner, "Enter your middle name (if none: N/A):");
+        String lastName = readString(myScanner,"Enter your last name");
+
+        if (middleName.equalsIgnoreCase("N/A")) {
+            System.out.println(firstName + " " + lastName);
+        } else System.out.println(firstName + " " + middleName + " " + lastName);
+
+    }
+
+    public static String readString(Scanner in, String prompt) {
+        System.out.println(prompt);
+        return in.nextLine();
+    }
 }
